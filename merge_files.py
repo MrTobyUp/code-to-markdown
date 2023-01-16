32d1ceb7189df87d8e9be55957573eee362bc04f
Toby <43244936+MrTobyUp@users.noreply.github.com>
2023-01-16 14:40:37 +0100

    Add files via upload

---
+++ merge_files.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+# specify the destination file
+destination_file = './output.txt'
+
+def copy_content(dir_path):
+    for filename in os.listdir(dir_path):
+        # ignore hidden files
+        if not filename.startswith('.'):
+            file_path = os.path.join(dir_path, filename)
+            if os.path.isfile(file_path):
+                with open(file_path, 'r') as infile:
+                    # write the contents of each file to the destination file
+                    with open(destination_file, 'a') as outfile:
+                        file_name = file_path.split("\\")[-1]
+                        outfile.write(file_name + ":\n```c\n")
+                        outfile.write(infile.read() + "```\n\n")
+            elif os.path.isdir(file_path):
+                copy_content(file_path)
+
+try:
+    directory = sys.argv[1]
+
+    with open(destination_file, 'w') as outfile:
+        copy_content(directory)
+except:
+    print("No valid destination directory passed via command line argument!")
